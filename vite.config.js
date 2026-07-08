c1417754b7ee66a23a42fa1f1e0ba37effb08f23
marie <choco10ciel@gmail.com>
2026-07-08 21:37:54 +0900

    add rsvpAndComp

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        rsvp: "rsvp.html",
+        complete: "rsvpComplete.html",
+        rsvpList: "rsvpList.html"
+      }
+    }
+  }
+});
